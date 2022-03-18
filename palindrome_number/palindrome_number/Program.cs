b2cdc584d166849fc3ef62cbf7fe5f288b62835a
Ghyad Adnan <ghayd.adnan2311@gmail.com>
2022-03-19 01:53:40 +0200

    Palindrome Number Task1 Update

--- palindrome_number/palindrome_number/Program.cs
+++ palindrome_number/palindrome_number/Program.cs
@@ -4,30 +4,12 @@ namespace palindrome_number
 {
     internal class Program
     {
-        public class Solution
-        {
-            public bool IsPalindrome(int x)
-            {
-                int sum = 0, d, num;
-                num = x;
-                while (x > 0)
-                {
-                    d = x % 10;
-                    x = x / 10;
-                    sum = (sum * 10) + d;
-                }
-                if (sum == num)
-                {
-                    return true;
-                }
-                return false;
-            }
-        }
+        
         static void Main(string[] args)
         {
-            Solution s = new Solution();    
-            int x = 121;
-            bool result = s.IsPalindrome(x);
+            Solution sol = new Solution();    
+            var number = 121;
+            bool result = sol.IsPalindrome(number);
             Console.WriteLine(result);
 
         }
---
+++ palindrome_number/palindrome_number/Solution.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace palindrome_number
+{
+    public class Solution
+    {
+        public bool IsPalindrome(int number)
+        {
+            var sum = 0;
+            var lastDigit = 0;
+            var dividedNumber = number;
+            while (dividedNumber > 0)
+            {
+                lastDigit = dividedNumber % 10;
+                dividedNumber = dividedNumber / 10;
+                sum = (sum * 10) + lastDigit;
+            }
+            if (sum == number)
+            {
+                return true;
+            }
+            return false;
+        }
+    }
+}
